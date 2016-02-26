749586829a99a27c270cc8d44127a1645bde73fe
Michael Wang <mwwmail@gmail.com>
2016-02-26 18:23:23 -0500

    Fix assoc_options to be retrieved in define_method instead

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,9 +5,9 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    through_options = assoc_options[through_name]
 
     define_method(name) do
+      through_options = self.class.assoc_options[through_name]
       source_options = through_options.model_class.assoc_options[source_name]
 
       through_foreign = through_options.foreign_key
